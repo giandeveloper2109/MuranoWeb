2c45ec681f7720b8f29ff3b5e065c27c1d4fd3ab
Giancarlo Joel Alvarez Gallardo <gianalvarezgallardo21@gmail.com>
2023-12-25 13:23:55 -0500

    Primera Subida Murano Web

---
+++ src/layouts/default/AppBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app-bar flat app class=" text-center d-flex flex-column">
+    <v-divider :thickness="20" class="border-opacity-0" vertical></v-divider>
+    <div>
+      <v-img class="mr-3" src="@/assets/logoMurano.png" height="200px" width="200px"></v-img>
+    </div>
+    <v-spacer></v-spacer>
+    <div>
+      <v-btn text style="color: rgba(11, 31, 145, 0.772);">Contactanos</v-btn>
+    </div>
+    <v-divider :thickness="7" class="border-opacity-0" vertical></v-divider>
+    <div>
+      <v-btn text style="color: rgba(11, 31, 145, 0.772);">Proyectos</v-btn>
+    </div>
+    <v-divider :thickness="7" class="border-opacity-0" vertical></v-divider>
+    <div>
+      <v-btn text style="color: rgba(11, 31, 145, 0.772);">Nosotros</v-btn>
+    </div>
+    <v-divider :thickness="7" class="border-opacity-0" vertical></v-divider>
+    <div>
+      <v-btn text style="color: rgba(11, 31, 145, 0.772);">Cotiza</v-btn>
+    </div>
+    <v-divider :thickness="7" class="border-opacity-0" vertical></v-divider>
+  </v-app-bar>
+</template>
+
+<script setup>
+  //
+</script>
