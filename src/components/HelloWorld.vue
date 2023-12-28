f3dc5f5d9bd250cfadc001535a796512c74a0e65
Diana Campos Del Carpio <146885700+Diana071194@users.noreply.github.com>
2023-12-28 14:55:27 -0500

    Primer cambio de Diana

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,16 +5,16 @@
     <v-responsive class="align-center text-center fill-height">
       <v-img height="300" src="@/assets/construccion.png" />
 
-      <div class="text-body-2 font-weight-light mb-n1">Bienvenido a Murano</div>
+      <div class="text-h4 font-weight-light mb-n1">Bienvenidos a Murano</div>
 
-      <h1 class="text-h2 font-weight-bold">Regresaremos pronto</h1>
+      <h1 class="text-h3 font-weight-bold">Estamos en Construcción.</h1>
 
       <div class="py-14" />
 
     </v-responsive>
       </v-container>
     </v-col>
-    
+
     <v-col>
       <v-container >
     <v-responsive class="align-center text-center fill-height">
